f2339bb47e6dcb7dc0322dd14dde6e6015e96ea8
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-02-20 18:59:37 +0200

    updated AirTemperatureGroup class

--- src/Sheme/AirTemperatureGroup.php
+++ src/Sheme/AirTemperatureGroup.php
@@ -3,6 +3,7 @@
 
 namespace Synop\Sheme;
 
+use Synop\Decoder\DecoderInterface;
 use Synop\Decoder\GroupDecoder\GroupDecoderInterface;
 use Synop\Sheme\GroupInterface;
 use Synop\Decoder\GroupDecoder\AirTemperatureDecoder;
@@ -38,6 +39,11 @@ class AirTemperatureGroup implements GroupInterface
      */
     private $temperature;
 
+    /**
+     * @var float The resulting signed air temperature
+     */
+    private $temperatureValue;
+
     public function __construct(string $data)
     {
         $this->setData($data);
@@ -61,6 +67,94 @@ class AirTemperatureGroup implements GroupInterface
     }
 
     /**
+     * Sets air temperature group data
+     * @param string $data temperature group data
+     */
+    public function setRawAirTemperature(string $data)
+    {
+        $this->raw_air_temperature = $data;
+    }
+
+    /**
+     * @param GroupDecoderInterface $decoder
+     */
+    public function setDecoder(GroupDecoderInterface $decoder)
+    {
+        $this->decoder = $decoder;
+    }
+
+    /**
+     * Sets air temperature sign
+     * @param int $singValue air temperature sign
+     */
+    public function setSignValue(int $singValue)
+    {
+        $this->sign = $singValue;
+    }
+
+    /**
+     * Sets air temperature in tenths value
+     * @param float $temperature air temperature value
+     */
+    public function setTemperatureData(float $temperature)
+    {
+        $this->temperature = $temperature;
+    }
+
+    /**
+     * Sets resulting signed air temperature
+     * @param float $temperatureValue resulting signed air temperature
+     */
+    public function setTemperatureValue(float $temperatureValue)
+    {
+        $this->temperatureValue = $temperatureValue;
+    }
+
+    /**
+     * Returns air temperature group data
+     * @return string Air temperature group data
+     */
+    public function getRawAirTemperature() : string
+    {
+        return $this->raw_air_temperature;
+    }
+
+    /**
+     * @return GroupDecoderInterface
+     */
+    public function getDecoder() : GroupDecoderInterface
+    {
+        return $this->decoder;
+    }
+
+    /**
+     * Returns air temperature sign
+     * @return int air temperature sign
+     */
+    public function getSignValue() : int
+    {
+        return $this->sign;
+    }
+
+    /**
+     * Returns air temperature in tenths
+     * @return float air temperature
+     */
+    public function getTemperatureData() : float
+    {
+        return $this->temperature;
+    }
+
+    /**
+     * Returns resulting signed air temperature
+     * @return float resulting signed air temperature
+     */
+    public function getTemperatureValue() : float
+    {
+        return $this->temperatureValue;
+    }
+
+    /**
      * Sets the values ​​for the air temperature group variables
      *
      * @param GroupDecoderInterface $decoder
@@ -70,9 +164,11 @@ class AirTemperatureGroup implements GroupInterface
         if ($this->isAirTempGroup($decoder)) {
             $this->setSign($decoder);
             $this->setTemperature($decoder);
+            $this->buildAirTemperature($this->sign, $this->temperature);
         } else {
             $this->setSign(null);
             $this->setTemperature(null);
+            $this->buildAirTemperature(null, null);
         }
     }
 
@@ -112,6 +208,24 @@ class AirTemperatureGroup implements GroupInterface
             $this->temperature = null;
         } else {
             $this->temperature = $decoder->getTemperatureValue();
+        }
+    }
+
+    /**
+     * Sets the full value of the signed air temperature
+     * @param int|null $sign air temperature sign
+     * @param float|null $airTemp air temperature in tenths
+     */
+    public function buildAirTemperature(?int $sign, ?float $airTemp)
+    {
+        if (!is_null($sign) && !is_null($airTemp)) {
+            if ($sign == 1) {
+                $this->temperatureValue = $airTemp * -1;
+            } else {
+                $this->temperatureValue = $airTemp;
+            }
+        } else {
+            $this->temperatureValue = null;
         }
     }
 }
