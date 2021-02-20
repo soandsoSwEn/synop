3f42ce8827d11fb92a2c84461a20afb73ba25354
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-02-20 19:01:57 +0200

    added MinAirTemperatureGroup class

---
+++ src/Sheme/MinAirTemperatureGroup.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Synop\Sheme;
+
+
+use Exception;
+use Synop\Decoder\GroupDecoder\AirTemperatureDecoder;
+
+/**
+ * Class MinAirTemperatureGroup contains methods for working with a group of minimum air temperatures
+ * of section three - 333 2SnTnTnTn
+ *
+ * @package Synop\Sheme
+ *
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+class MinAirTemperatureGroup extends AirTemperatureGroup
+{
+    /** Value distinctive number of minimum air temperature group */
+    const DIGIT = '2';
+
+    public function __construct(string $data)
+    {
+        parent::__construct($data);
+    }
+
+    /**
+     * Sets the initial data for the minimum air temperature group
+     * @param string $data Minimum air temperature group data
+     * @throws Exception
+     */
+    public function setData(string $data)
+    {
+        if(!empty($data)) {
+            $this->setRawAirTemperature($data);
+            $this->setDecoder(new AirTemperatureDecoder($this->getRawAirTemperature(), self::DIGIT));
+            $this->setAirTempGroup($this->getDecoder());
+        } else {
+            throw new Exception('Minimum Air Temperature Group group cannot be empty!');
+        }
+    }
+}
