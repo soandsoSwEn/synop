d1fd64bbb3d36748a81899196e3dbc98e3f29578
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-03-05 12:49:17 +0200

    added GroundWithSnowGroup class

---
+++ src/Decoder/GroupDecoder/GroundWithSnowDecoder.php
@@ -0,0 +1,112 @@
+<?php
+
+
+namespace Synop\Decoder\GroupDecoder;
+
+
+use Exception;
+
+/**
+ * Class GroundWithSnowDecoder contains methods for decoding a group of state of the ground with snow
+ * or measurable ice cover
+ *
+ * @package Synop\Decoder\GroupDecoder
+ *
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+class GroundWithSnowDecoder implements GroupDecoderInterface
+{
+    /** Distinctive digit of state of ground with snow or measurable ice cover group */
+    const DIGIT = '4';
+
+    /**
+     * @var string State of ground with snow data
+     */
+    private $rawGroundWithSnow;
+
+    /**
+     * @var string[] Map correspondences of symbolic and state of ground with snow or measurable ice cover values
+     */
+    private $groundStateMap = [
+        '0' => 'Ground predominantly covered by ice',
+        '1' => 'Compact or wet snow (with or without ice) covering less than one-half of the ground',
+        '2' => 'Compact or wet snow (with or without ice) covering at least one-half of the ground but ground not
+                completely covered',
+        '3' => 'Even layer of compact or wet snow covering ground completely',
+        '4' => 'Uneven layer of compact or wet snow covering ground completely',
+        '5' => 'Loose dry snow covering less than one-half of the ground',
+        '6' => 'Loose dry snow covering at least one-half of the ground (but not completely)',
+        '7' => 'Even layer of loose dry snow covering ground completely',
+        '8' => 'Uneven layer of loose dry snow covering ground completely',
+        '9' => 'Snow covering ground completely; deep drifts',
+    ];
+
+    /**
+     * @var string[] Map correspondences of symbolic and depth of snow string values
+     */
+    private $depthSnowMap = [
+        '997' => 'Less than 0.5 cm',
+        '998' => 'Snow cover, not continuous',
+        '999' => 'Measurement impossible or inaccurate',
+    ];
+
+    public function __construct(string $rawGroundWithSnow)
+    {
+        $this->rawGroundWithSnow = $rawGroundWithSnow;
+    }
+
+    /**
+     * Returns the result of checking the validity of the group
+     * @return bool
+     */
+    public function isGroup() : bool
+    {
+        $distinguishingDigit = substr($this->rawGroundWithSnow, 0, 1);
+
+        return strcasecmp($distinguishingDigit, self::DIGIT) == 0;
+    }
+
+    /**
+     * Return code figure of state ground
+     * @return int Code figure of state of the ground with snow or measurable ice cover
+     * @throws Exception
+     */
+    public function getCodeGroundState() : int
+    {
+        $E = substr($this->rawGroundWithSnow, 1, 1);
+        if (array_key_exists($E, $this->groundStateMap)) {
+            return intval($E);
+        } else {
+            throw new Exception('Invalid data Code figure of State ground with snow');
+        }
+    }
+
+    /**
+     * Return state of ground title
+     * @return string State of ground with snow or measurable ice cover
+     * @throws Exception
+     */
+    public function getGroundState() : string
+    {
+        $E = substr($this->rawGroundWithSnow, 1, 1);
+        if (array_key_exists($E, $this->groundStateMap)) {
+            return $this->groundStateMap[$E];
+        } else {
+            throw new Exception('Invalid data of State ground with snow');
+        }
+    }
+
+    /**
+     * Returns depth of snow data
+     * @return array|string[] Depth of snow data value
+     */
+    public function getDepthSnow() : array
+    {
+        $sss = substr($this->rawGroundWithSnow, 2, 3);
+        if (array_key_exists($sss, $this->depthSnowMap)) {
+            return ['value' => $this->depthSnowMap[$sss]];
+        }
+
+        return ['value' => intval($sss)];
+    }
+}
---
+++ src/Sheme/GroundWithSnowGroup.php
@@ -0,0 +1,218 @@
+<?php
+
+
+namespace Synop\Sheme;
+
+
+use Exception;
+use Synop\Decoder\GroupDecoder\GroundWithSnowDecoder;
+use Synop\Decoder\GroupDecoder\GroupDecoderInterface;
+
+/**
+ * Class GroundWithSnowGroup contains methods for working with a group of state of the ground with snow
+ * or measurable ice cover of section three - 333 4Esss
+ *
+ * @package Synop\Sheme
+ *
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+class GroundWithSnowGroup implements GroupInterface
+{
+    /**
+     * @var string State of ground with snow or measurable ice cover group data
+     */
+    private $raw_ground_with_snow;
+
+    /**
+     * @var GroupDecoderInterface Initialized decoder object
+     */
+    private $decoder;
+
+    /**
+     * @var int Code figure of state of ground
+     */
+    private $codeState;
+
+    /**
+     * @var string State of ground title
+     */
+    private $state;
+
+    /**
+     * @var array Depth of snow
+     */
+    private $depthSnow;
+
+    public function __construct(string $data)
+    {
+        $this->setData($data);
+    }
+
+    /**
+     * Sets the initial data for state of ground group
+     * @param string $data state of ground with snow or measurable ice cover group data
+     * @throws Exception
+     */
+    public function setData(string $data)
+    {
+        if (!empty($data)) {
+            $this->setRawGroundWithSnow($data);
+            $this->setDecoder(new GroundWithSnowDecoder($this->getRawGroundWithSnow()));
+            $this->setGroundWithSnowGroup($this->getDecoder());
+        } else {
+            throw new Exception('GroundWithSnowGroup group cannot be empty!');
+        }
+    }
+
+    /**
+     * Sets state of ground with snow or measurable ice cover group data
+     * @param string $data State of ground with snow or measurable ice cover
+     */
+    public function setRawGroundWithSnow(string $data) : void
+    {
+        $this->raw_ground_with_snow = $data;
+    }
+
+    public function setDecoder(GroupDecoderInterface $decoder) : void
+    {
+        $this->decoder = $decoder;
+    }
+
+    /**
+     * Sets code figure of state ground with snow group
+     * @param int|null $codeState Code figure of state ground with snow
+     */
+    public function setCodeStateValue(?int $codeState) : void
+    {
+        $this->codeState = $codeState;
+    }
+
+    /**
+     * Sets state ground with snow value
+     * @param string|null $state State ground with snow
+     */
+    public function setStateValue(?string $state) : void
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * Sets depth of snow value
+     * @param array|null $depthSnow Depth of snow
+     */
+    public function setDepthSnowValue(?array $depthSnow) : void
+    {
+        $this->depthSnow = $depthSnow;
+    }
+
+    /**
+     * Returns state of ground with snow or measurable ice cover group data
+     * @return string State of ground with snow or measurable ice cover group
+     */
+    public function getRawGroundWithSnow() : string
+    {
+        return $this->raw_ground_with_snow;
+    }
+
+    /**
+     * Returns initialized decoder object for state of ground with snow group
+     * @return GroupDecoderInterface Decoder object for state of ground with snow group
+     */
+    public function getDecoder() : GroupDecoderInterface
+    {
+        return $this->decoder;
+    }
+
+    /**
+     * Returns code figure of state ground with snow group
+     * @return int|null Code figure of state ground with snow group
+     */
+    public function getCodeStateValue() : ?int
+    {
+        return $this->codeState;
+    }
+
+    /**
+     * Returns state ground with snow value
+     * @return int|null State ground with snow
+     */
+    public function getStateValue() : ?int
+    {
+        return $this->state;
+    }
+
+    /**
+     * Returns depth of snow value
+     * @return array|null Depth of snow
+     */
+    public function getDepthSnowValue() : ?array
+    {
+        return $this->depthSnow;
+    }
+
+    /**
+     * Returns whether the given group is a state of ground with snow group
+     * @param GroupDecoderInterface $decoder Initialized decoder object for state of ground with snow group
+     */
+    public function setGroundWithSnowGroup(GroupDecoderInterface $decoder)
+    {
+        if ($this->isDrWthSnGroup($decoder)) {
+            $this->setCodeState($decoder);
+            $this->setState($decoder);
+            $this->setDepthSnow($decoder);
+        } else {
+            $this->setCodeState(null);
+            $this->setState(null);
+            $this->setDepthSnow(null);
+        }
+    }
+
+    /**
+     * Returns whether the given group is a state of ground with snow group
+     * @param GroupDecoderInterface $decoder Initialized decoder object
+     * @return bool
+     */
+    public function isDrWthSnGroup(GroupDecoderInterface $decoder) : bool
+    {
+        return $decoder->isGroup();
+    }
+
+    /**
+     * Sets Code figure of state ground with snow
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setCodeState(?GroupDecoderInterface $decoder) : void
+    {
+        if (is_null($decoder)) {
+            $this->setCodeStateValue(null);
+        } else {
+            $this->setCodeStateValue($decoder->getCodeGroundState());
+        }
+    }
+
+    /**
+     * Sets state ground with snow
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setState(?GroupDecoderInterface $decoder) : void
+    {
+        if (is_null($decoder)) {
+            $this->setStateValue(null);
+        } else {
+            $this->setStateValue($decoder->getGroundState());
+        }
+    }
+
+    /**
+     * Sets depth of snow
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setDepthSnow(?GroupDecoderInterface $decoder) : void
+    {
+        if (is_null($decoder)) {
+            $this->setDepthSnowValue(null);
+        } else {
+            $this->setDepthSnowValue($decoder->getDepthSnow());
+        }
+    }
+}
