a01c6f15245856719e1b8ce08c265a4b045a3e9a
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-03-09 18:19:00 +0200

    added AdditionalCloudInformationGroup class

---
+++ src/Decoder/GroupDecoder/AdditionalCloudInformationDecoder.php
@@ -0,0 +1,215 @@
+<?php
+
+
+namespace Synop\Decoder\GroupDecoder;
+
+
+use Exception;
+
+/**
+ * Class AdditionalCloudInformationDecoder contains methods for decoding a group of Additional cloud
+ * information transfer data
+ *
+ * @package Synop\Decoder\GroupDecoder
+ *
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+class AdditionalCloudInformationDecoder implements GroupDecoderInterface
+{
+    /** Value distinctive number of Additional cloud information transfer group */
+    const DIGIT = '8';
+
+    /**
+     * @var string Additional cloud information transfer data
+     */
+    private $rawAdditionCloudInformation;
+
+    /**
+     * @var string[] Map correspondences of symbolic and amount of individual cloud layer values
+     */
+    private $amountCloudMap = [
+        '0' => 'Niel',
+        '1' => '1 eight of sky covered, or less, but not zero',
+        '2' => '2 eight of sky covered',
+        '3' => '3 eight of sky covered',
+        '4' => '4 eight of sky covered',
+        '5' => '5 eight of sky covered',
+        '6' => '6 eight of sky covered',
+        '7' => '7 eight of sky covered, or more, but not completely covered',
+        '8' => 'Sky completely covered',
+        '9' => 'Sky obscured or cloud amount cannot be estimated',
+    ];
+
+    /**
+     * @var string[] Map correspondences of symbolic and form of cloud values
+     */
+    private $formCloudMap = [
+        '0' => 'Cirrus (Ci)',
+        '1' => 'Cirrocumulus (Cc)',
+        '2' => 'Cirrostratus (Cs)',
+        '3' => 'Altocumulus (Ac)',
+        '4' => 'Altostratus (As)',
+        '5' => 'Nimbostratus (Ns)',
+        '6' => 'Stratocumulus (Sc)',
+        '7' => 'Status (St)',
+        '8' => 'Cumulus (Cu)',
+        '9' => 'Cumulonimbus (Cb)',
+        '/' => 'Cloud not visible owing to darkness, fog, duststorm, sandstorm or other analogous phenomena',
+    ];
+
+    private $heightCloudMap = [
+        '90' => '< 50',
+        '91' => '50-100',
+        '92' => '100-200',
+        '93' => '200-300',
+        '94' => '300-600',
+        '95' => '600-1000',
+        '96' => '1000-1500',
+        '97' => '1500-2000',
+        '98' => '2000-2500',
+        '99' => '> 2500 or no clouds',
+    ];
+
+    public function __construct(string $rawAdditionCloudInformation)
+    {
+        $this->rawAdditionCloudInformation = $rawAdditionCloudInformation;
+    }
+
+    /**
+     * Returns the result of checking the validity of the group
+     * @return bool
+     */
+    public function isGroup() : bool
+    {
+        $distinguishingDigit = substr($this->rawAdditionCloudInformation, 0, 1);
+
+        return strcasecmp($distinguishingDigit, self::DIGIT) == 0;
+    }
+
+    /**
+     * Returns code figure of amount of individual cloud layer
+     * @return string|null Code figure of amount of individual cloud layer
+     * @throws Exception
+     */
+    public function getCodeAmountCloud() : ?string
+    {
+        $Ns = substr($this->rawAdditionCloudInformation, 1, 1);
+        if (array_key_exists($Ns, $this->amountCloudMap)) {
+            return $Ns;
+        } elseif (strcasecmp($Ns, '/') == 0) {
+            return null;
+        } else {
+            throw new Exception('Invalid data Code figure of amount of individual cloud layer');
+        }
+    }
+
+    /**
+     * Returns amount of individual cloud layer
+     * @return string|null Amount of individual cloud layer
+     * @throws Exception
+     */
+    public function getAmountCloud() : ?string
+    {
+        $Ns = $this->getCodeAmountCloud();
+        if (is_null($Ns)) {
+            return null;
+        }
+
+        return $this->amountCloudMap[$Ns];
+    }
+
+    /**
+     * Returns form of cloud
+     * @return string Form of cloud
+     * @throws Exception
+     */
+    public function getFormCloud()
+    {
+        $C = substr($this->rawAdditionCloudInformation, 2, 1);
+        if (array_key_exists($C, $this->formCloudMap)) {
+            return $this->formCloudMap[$C];
+        } else {
+            throw new Exception('Invalid data Form of Cloud');
+        }
+    }
+
+    /**
+     * Returns code figure of height of base of cloud layer
+     * @return false|string|null Code figure of height of base of cloud layer
+     */
+    public function getCodeHeightCloud() : ?string
+    {
+        $hshs = substr($this->rawAdditionCloudInformation, 3, 2);
+        if (strcasecmp($hshs, '//') == 0) {
+            return null;
+        }
+
+        return $hshs;
+    }
+
+    /**
+     * Returns height of base of cloud layer
+     * @return float[]|string[]|null Height of base of cloud layer data
+     * @throws Exception
+     */
+    public function getHeightCloud() : ?array
+    {
+        $hshs = $this->getCodeHeightCloud();
+        if (is_null($hshs)) {
+            return null;
+        }
+
+        if (array_key_exists($hshs, $this->heightCloudMap)) {
+            return ['Height' => $this->heightCloudMap[$hshs]];
+        }
+
+        $intValueOfHshs = intval($hshs);
+        if ($intValueOfHshs >=0 && $intValueOfHshs <= 50) {
+            return ['Height' => $this->get0050Height($intValueOfHshs)];
+        } elseif ($intValueOfHshs >= 56 && $intValueOfHshs <= 80) {
+            return ['Height' => $this->get5080Height($intValueOfHshs)];
+        } elseif ($intValueOfHshs >= 81 && $intValueOfHshs <= 89) {
+            return ['Height' => $this->get8189Height($intValueOfHshs)];
+        } else {
+            throw new Exception('Invalid data Height of base of cloud layer');
+        }
+    }
+
+    /**
+     * Returns the value of the height of base of cloud for cases of heights between 0 and 1500 m
+     * @param int $intValueOfHshs Code figure of height of base of cloud layer
+     * @return float|int|string
+     */
+    public function get0050Height(int $intValueOfHshs)
+    {
+        if ($intValueOfHshs == 0) {
+            return '< 30';
+        }
+
+        return $intValueOfHshs * 30;
+    }
+
+    /**
+     * Returns the value of the height of base of cloud for cases of heights between 1800 and 9000 m
+     * @param int $intValueOfHshs Code figure of height of base of cloud layer
+     * @return float|int
+     */
+    public function get5080Height(int $intValueOfHshs)
+    {
+        return ($intValueOfHshs - 50) * 300;
+    }
+
+    /**
+     * Returns the height of base of cloud for cases of altitude between 10500 and over 21000 m
+     * @param int $intValueOfHshs Code figure of height of base of cloud layer
+     * @return float|int|string
+     */
+    public function get8189Height(int $intValueOfHshs)
+    {
+        if ($intValueOfHshs == 89) {
+            return '> 21000';
+        }
+
+        return ($intValueOfHshs - 80) * 1500 + 9000;
+    }
+}
---
+++ src/Sheme/AdditionalCloudInformationGroup.php
@@ -0,0 +1,294 @@
+<?php
+
+
+namespace Synop\Sheme;
+
+
+use Exception;
+use Synop\Decoder\GroupDecoder\AdditionalCloudInformationDecoder;
+use Synop\Decoder\GroupDecoder\GroupDecoderInterface;
+
+/**
+ * Class AdditionalCloudInformationGroup contains methods for working with a group Additional cloud information
+ * transfer data of section three - 333 8NsChshs
+ *
+ * @package Synop\Sheme
+ *
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+class AdditionalCloudInformationGroup implements GroupInterface
+{
+    /**
+     * @var string Additional cloud information transfer group data
+     */
+    private $rawAdditionCloudInformation;
+
+    /**
+     * @var GroupDecoderInterface Initialized decoder object
+     */
+    private $decoder;
+
+    /**
+     * @var string Code figure of amount of individual cloud layer
+     */
+    private $codeAmountCloud;
+
+    /**
+     * @var string Amount of individual cloud layer
+     */
+    private $amountCloud;
+
+    /**
+     * @var string Form of Cloud
+     */
+    private $formCloud;
+
+    /**
+     * @var string Code figure of height of base of cloud layer
+     */
+    private $codeHeightCloud;
+
+    /**
+     * @var array Height of base of cloud layer
+     */
+    private $heightCloud;
+
+    public function __construct(string $data)
+    {
+        $this->setData($data);
+    }
+
+    /**
+     * Sets the initial data for additional cloud information transfer group
+     * @param string $data Additional cloud information transfer group data
+     * @throws Exception
+     */
+    public function setData(string $data)
+    {
+        if (!empty($data)) {
+            $this->setRawAdditionCloudInformation($data);
+            $this->setDecoder(new AdditionalCloudInformationDecoder($this->getRawAdditionCloudInformation()));
+            $this->setAdditionalCloudInformationGroup($this->getDecoder());
+        } else {
+            throw new Exception('AdditionalCloudInformationGroup group cannot be empty!');
+        }
+    }
+
+    /**
+     * Sets additional cloud information transfer group data
+     * @param string $data Additional cloud information transfer group data
+     */
+    public function setRawAdditionCloudInformation(string $data) : void
+    {
+        $this->rawAdditionCloudInformation = $data;
+    }
+
+    /**
+     * Sets an initialized decoder object for additional cloud information transfer group
+     * @param GroupDecoderInterface $decoder Initialized decoder object for additional cloud information transfer group
+     */
+    public function setDecoder(GroupDecoderInterface $decoder) : void
+    {
+        $this->decoder = $decoder;
+    }
+
+    /**
+     * Sets code figure of amount of individual cloud layer value
+     * @param string|null $codeAmountCloud Code figure of amount of individual cloud layer
+     */
+    public function setCodeAmountCloudValue(?string $codeAmountCloud) : void
+    {
+        $this->codeAmountCloud = $codeAmountCloud;
+    }
+
+    /**
+     * Sets amount of individual cloud layer value
+     * @param string|null $amountCloud Amount of individual cloud layer
+     */
+    public function setAmountCloudValue(?string $amountCloud) : void
+    {
+        $this->amountCloud = $amountCloud;
+    }
+
+    /**
+     * Sets Form of Cloud value
+     * @param string|null $formCloud Form of Cloud
+     */
+    public function setFormCloudValue(?string $formCloud) : void
+    {
+        $this->formCloud = $formCloud;
+    }
+
+    /**
+     * Sets code figure of height of base of cloud layer value
+     * @param string|null $codeHeightCloud Code figure of height of base of cloud layer
+     */
+    public function setCodeHeightCloudValue(?string $codeHeightCloud) : void
+    {
+        $this->codeHeightCloud = $codeHeightCloud;
+    }
+
+    /**
+     * Sets Height of base of cloud layer value
+     * @param array|null $codeHeight Height of base of cloud layer
+     */
+    public function setHeightCloudValue(?array $codeHeight) : void
+    {
+        $this->heightCloud = $codeHeight;
+    }
+
+    /**
+     * Returns additional cloud information transfer group data
+     * @return string Additional cloud information transfer group data
+     */
+    public function getRawAdditionCloudInformation() : string
+    {
+        return $this->rawAdditionCloudInformation;
+    }
+
+    /**
+     * Returns Initialized decoder object for additional cloud information transfer group
+     * @return GroupDecoderInterface Initialized decoder object for additional cloud information transfer group
+     */
+    public function getDecoder() : GroupDecoderInterface
+    {
+        return $this->decoder;
+    }
+
+    /**
+     * Returns code figure of amount of individual cloud layer value
+     * @return string|null Code figure of amount of individual cloud layer
+     */
+    public function getCodeAmountCloudValue() : ?string
+    {
+        return $this->codeAmountCloud;
+    }
+
+    /**
+     * Returns amount of individual cloud layer value
+     * @return string|null Amount of individual cloud layer
+     */
+    public function getAmountCloudValue() : ?string
+    {
+        return $this->amountCloud;
+    }
+
+    /**
+     * Returns Form of Cloud value
+     * @return string|null Form of Cloud
+     */
+    public function getFormCloudValue() : ?string
+    {
+        return $this->formCloud;
+    }
+
+    /**
+     * Returns Code figure of height of base of cloud layer value
+     * @return string|null Code figure of height of base of cloud layer
+     */
+    public function getCodeHeightCloudValue() : ?string
+    {
+        return $this->codeHeightCloud;
+    }
+
+    /**
+     * Returns height of base of cloud layer value
+     * @return array|null Height of base of cloud layer
+     */
+    public function getHeightCloudValue() : ?array
+    {
+        return $this->heightCloud;
+    }
+
+    public function setAdditionalCloudInformationGroup(GroupDecoderInterface $decoder) : void
+    {
+        if ($this->isAddCloudGroup($decoder)) {
+            $this->setCodeAmountCloud($decoder);
+            $this->setAmountCloud($decoder);
+            $this->setFormCloud($decoder);
+            $this->setCodeHeightCloud($decoder);
+            $this->setHeightCloud($decoder);
+        } else {
+            $this->setCodeAmountCloud(null);
+            $this->setAmountCloud(null);
+            $this->setFormCloud(null);
+            $this->setCodeHeightCloud(null);
+            $this->setHeightCloud(null);
+        }
+    }
+
+    /**
+     * Returns whether the given group is an additional cloud information transfer group
+     * @param GroupDecoderInterface $decoder Initialized decoder object
+     * @return bool
+     */
+    public function isAddCloudGroup(GroupDecoderInterface $decoder) : bool
+    {
+        return $decoder->isGroup();
+    }
+
+    /**
+     * Sets code figure of amount of individual cloud layer
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setCodeAmountCloud(?GroupDecoderInterface $decoder) : void
+    {
+        if (is_null($decoder)) {
+            $this->setCodeAmountCloudValue(null);
+        } else {
+            $this->setCodeAmountCloudValue($decoder->getCodeAmountCloud());
+        }
+    }
+
+    /**
+     * Sets amount of individual cloud layer
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setAmountCloud(?GroupDecoderInterface $decoder) : void
+    {
+        if (is_null($decoder)) {
+            $this->setAmountCloudValue(null);
+        } else {
+            $this->setAmountCloudValue($decoder->getAmountCloud());
+        }
+    }
+
+    /**
+     * Sets Form of Cloud
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setFormCloud(?GroupDecoderInterface $decoder) : void
+    {
+        if (is_null($decoder)) {
+            $this->setFormCloudValue(null);
+        } else {
+            $this->setFormCloudValue($decoder->getFormCloud());
+        }
+    }
+
+    /**
+     * Sets code figure of height of base of cloud layer
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setCodeHeightCloud(?GroupDecoderInterface $decoder)
+    {
+        if (is_null($decoder)) {
+            $this->setCodeHeightCloudValue(null);
+        } else {
+            $this->setCodeHeightCloudValue($decoder->getCodeHeightCloud());
+        }
+    }
+
+    /**
+     * Sets Height of base of cloud layer
+     * @param GroupDecoderInterface|null $decoder Initialized decoder object
+     */
+    public function setHeightCloud(?GroupDecoderInterface $decoder) : void
+    {
+        if (is_null($decoder)) {
+            $this->setHeightCloudValue(null);
+        } else {
+            $this->setHeightCloudValue($decoder->getHeightCloud());
+        }
+    }
+}
