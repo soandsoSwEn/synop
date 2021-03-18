c2449f91f7aad21fe62bb14a5d9af0530767d10b
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-03-18 18:14:59 +0200

    updated GroupInterface

--- src/Sheme/GroupInterface.php
+++ src/Sheme/GroupInterface.php
@@ -2,11 +2,36 @@
 
 namespace Synop\Sheme;
 
+use Synop\Decoder\GroupDecoder\GroupDecoderInterface;
+
 /**
+ * This interface should be implemented by a class that creates a group of code of weather report.
+ *
+ * @package Synop\Sheme
  *
  * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
  */
 interface GroupInterface
 {
-    //put your code here
+    /**
+     * Sets decoder object for the given group of code of weather report
+     *
+     * @param GroupDecoderInterface $decoder Decoder object for the group of code of weather report
+     */
+    public function setDecoder(GroupDecoderInterface $decoder) : void;
+
+    /**
+     * Sets the initial data for a weather report group and prepares for use
+     *
+     * @param string $data Raw symbol data of group of weather report
+     */
+    public function setData(string $data) : void;
+
+    /**
+     * Returns decoder object for the given group of code of weather report
+     *
+     * @return GroupDecoderInterface Decoder object for the group of code of weather report
+     */
+    public function getDecoder() : GroupDecoderInterface;
+
 }
