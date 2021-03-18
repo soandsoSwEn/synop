89e4140f8014a6aa720128194289c0fe76383f5d
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-03-18 17:07:43 +0200

    added get fh for part data in Report class

--- src/Report.php
+++ src/Report.php
@@ -2,6 +2,7 @@
 
 namespace Synop;
 
+use Synop\Fabrication\PartData;
 use Synop\ReportInterface;
 use Exception;
 use Synop\Fabrication\Validate;
@@ -9,6 +10,7 @@ use Synop\Fabrication\RawReport;
 use Synop\Decoder\GeneralDecoder;
 use Synop\Sheme\Section;
 use Synop\Process\Pipeline;
+use Synop\Sheme\SectionInterface;
 
 /**
  * Weather report initial processing
@@ -29,10 +31,21 @@ class Report implements ReportInterface
      */
     private $raw_report;
 
+    /**
+     * @var SectionInterface
+     */
+    private $rawBlocksData;
+
+    /**
+     * @var PartData
+     */
+    private $partData;
+
 
     public function __construct(string $report)
     {
         $this->setReport($report);
+        $this->partData = new PartData();
     }
 
     public function setReport(string $report): void
@@ -81,7 +94,8 @@ class Report implements ReportInterface
         $pipeline = new Pipeline();
         $pipeline->pipe($pipes);
         $decoder = new GeneralDecoder(new Section(self::GENERAL_SECTION));
-        $blocks =  $pipeline->process($this->raw_report, $decoder); var_dump($blocks);
+        $blocks =  $pipeline->process($this->raw_report, $decoder);
+        $this->rawBlocksData = $blocks;
     }
     
     private function getPipes() : array
@@ -117,6 +131,414 @@ class Report implements ReportInterface
             '444',
             '555',
         ];
+    }
+
+    //Get Part of Data
+
+    /**
+     * Get a type of weather station
+     * @return string|null
+     */
+    public function getType1() : ?string
+    {
+        return $this->partData->getTypeStation($this->rawBlocksData);
+    }
+
+    /**
+     * Get a date (day) of a meteorological report
+     * @return string|null
+     */
+    public function getDay() : ?string
+    {
+        return $this->partData->getDayReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a time (hour) of a meteorological report
+     * @return string|null
+     */
+    public function getTime() : ?string
+    {
+        return $this->partData->getHourReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a unit of measure for wind speed
+     * @return string|null
+     */
+    public function getUnitWind() : ?string
+    {
+        return $this->partData->getUnitWindReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a type of wind measurement
+     * @return string|null
+     */
+    public function getWindDetection() : ?string
+    {
+        return $this->partData->getWindDetectionReport($this->rawBlocksData);
+    }
+
+    //Station Group IIiii
+
+    /**
+     * Get area number of meteorological station
+     * @return string|null
+     */
+    public function getAreaNumber() : ?string
+    {
+        return $this->partData->getAreaNumberReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a number of meteorological station
+     * @return string|null
+     */
+    public function getStationNumber() : ?string
+    {
+        return $this->partData->getStationNumberReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a station index
+     * @return string|null
+     */
+    public function getStationIndex() : ?string
+    {
+        return $this->partData->getStationIndexReport($this->rawBlocksData);
+    }
+
+    //Group IrIxHvv
+
+    /**
+     * Get index inclusion the precipitation group 6RRRtr
+     * @return string|null
+     */
+    public function getInclusionPrecipitation() : ?string
+    {
+        return $this->partData->getInclusionPrecipitationReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a weather indicator inclusion index 7wwW1W2
+     * @return string|null
+     */
+    public function getInclusionWeather() : ?string
+    {
+        return $this->partData->getInclusionWeatherReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a type station operation
+     * @return string|null
+     */
+    public function getTypeStationOperation() : ?string
+    {
+        return $this->partData->getTypeStationOperationReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a base height of low clouds above sea level
+     * @return string|null
+     */
+    public function getHeightLowCloud() : ?string
+    {
+        return $this->partData->getHeightLowCloudReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a meteorological range of visibility
+     * @return string|null
+     */
+    public function getVisibility() : ?string
+    {
+        return $this->partData->getVisibilityReport($this->rawBlocksData);
+    }
+
+    //Group NDDff
+
+    /**
+     * Get total clouds
+     * @return string|null
+     */
+    public function getTotalAmountCloud() : ?string
+    {
+        return $this->partData->getTotalAmountCloudReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get direction of wind
+     * @return string|null
+     */
+    public function getWindDirection() : ?string
+    {
+        return $this->partData->getWindDirectionReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get wind speed
+     * @return string|null
+     */
+    public function getWindSpeed() : ?string
+    {
+        return $this->partData->getWindSpeedReport($this->rawBlocksData);
+    }
+
+    //Group 1SnTTT
+
+    /**
+     * Get air temperature
+     * @return float|null
+     */
+    public function getAirTemperature() : ?float
+    {
+        return $this->partData->getAirTemperatureReport($this->rawBlocksData);
+    }
+
+    //Group 2SnTdTdTd
+
+    /**
+     * Get dew point temperature
+     * @return float|null
+     */
+    public function getDewPointTemperature() : ?float
+    {
+        return $this->partData->getDewPointTemperatureReport($this->rawBlocksData);
+    }
+
+    //Group 3P0P0P0
+
+    /**
+     * Get atmospheric pressure at station level
+     * @return float|null
+     */
+    public function getStationLevelPressure() : ?float
+    {
+        return $this->partData->getStationLevelPressureReport($this->rawBlocksData);
+    }
+
+    //Group 4PPPP
+
+    /**
+     * Get atmospheric pressure reduced to mean sea level
+     * @return float|null
+     */
+    public function getSeaLevelPressure() : ?float
+    {
+        return $this->partData->getSeaLevelPressureReport($this->rawBlocksData);
+    }
+
+    //Group 5appp
+
+    /**
+     * Get pressure change over last three hours
+     * @return string|null
+     */
+    public function getBaricTendency() : ?string
+    {
+        return $this->partData->getBaricTendencyReport($this->rawBlocksData);
+    }
+
+    //Group 6RRRtr
+
+    /**
+     * Get a title or value of amount of rainfall
+     * @return mixed|null
+     */
+    public function getAmountRainfall()
+    {
+        return $this->partData->getAmountRainfallReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get duration period of rainfall
+     * @return string|null
+     */
+    public function getDurationPeriodRainfall() : ?string
+    {
+        return $this->partData->getDurationPeriodRainfallReport($this->rawBlocksData);
+    }
+
+    //Group 7wwW1W2
+
+    /**
+     * Get present weather
+     * @return string|null
+     */
+    public function getPresentWeather() : ?string
+    {
+        return $this->partData->getPresentWeatherReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get Past weather
+     * @return array|null
+     */
+    public function getPastWeather() : ?array
+    {
+        return $this->partData->getPastWeatherReport($this->rawBlocksData);
+    }
+
+    //Group 8NhClCmCH
+
+    /**
+     * Get amount of low or middle cloud
+     * @return string|null
+     */
+    public function getAmountLowOrMiddleCloud() : ?string
+    {
+        return $this->partData->getAmountLowOrMiddleCloudReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get form of low cloud
+     * @return string|null
+     */
+    public function getFormLowCloud() : ?string
+    {
+        return $this->partData->getFormLowCloudReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get form of medium cloud
+     * @return string|null
+     */
+    public function getFormMediumCloud() : ?string
+    {
+        return $this->partData->getFormMediumCloudReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get form of high cloud
+     * @return string|null
+     */
+    public function getFormHighCloud() : ?string
+    {
+        return $this->partData->getFormHighCloudReport($this->rawBlocksData);
+    }
+
+    //Section 3
+    //Group 1SnTxTxTx
+
+    /**
+     * Get maximum air temperature
+     * @return float|null
+     */
+    public function getMaxAirTemperature() : ?float
+    {
+        return $this->partData->getMaxAirTemperatureReport($this->rawBlocksData);
+    }
+
+    //Group 2SnTnTnTn
+
+    /**
+     * Get minimum air temperature
+     * @return float|null
+     */
+    public function getMinAirTemperature() : ?float
+    {
+        return $this->partData->getMinAirTemperatureReport($this->rawBlocksData);
+    }
+
+    //Group 3ESnTgTg
+
+    /**
+     * Get a state of ground for case ground without snow or measurable ice cover
+     * @return string|null
+     */
+    public function getStateGroundWithoutSnow() : ?string
+    {
+        return $this->partData->getStateGroundWithoutSnowReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get grass minimum temperature for case ground without snow or measurable ice cover
+     * @return int|null
+     */
+    public function getMinTemperatureOfGroundWithoutSnow() : ?int
+    {
+        return $this->partData->getMinTemperatureOfGroundWithoutSnowReport($this->rawBlocksData);
+    }
+
+    //Group 4Esss
+
+    /**
+     * Get a state of ground for case ground with snow or measurable ice cover
+     * @return string|null
+     */
+    public function getStateGroundWithSnow() : ?string
+    {
+        return $this->partData->getStateGroundWithSnowReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get depth of snow
+     * @return mixed|null
+     */
+    public function getDepthSnow()
+    {
+        return $this->partData->getDepthSnowReport($this->rawBlocksData);
+    }
+
+    //Group 55SSS
+
+    /**
+     * Get a duration of daily sunshine
+     * @return float|null
+     */
+    public function getDurationSunshine() : ?float
+    {
+        return $this->partData->getDurationSunshineReport($this->rawBlocksData);
+    }
+
+    //Group 6RRRtr
+
+    /**
+     * Get amount of rainfall
+     * @return int|null
+     */
+    public function getRegionalExchangeAmountRainfall() : ?int
+    {
+        return $this->partData->getRegionalExchangeAmountRainfallReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get duration period of rainfall
+     * @return string|null
+     */
+    public function getRegionalExchangeDurationPeriodRainfall() : ?string
+    {
+        return $this->partData->getRegionalExchangeDurationPeriodRainfallReport($this->rawBlocksData);
+    }
+
+    //Group 8NsChshs
+
+    /**
+     * Get amount of individual cloud layer
+     * @return string|null
+     */
+    public function getAmountIndividualCloudLayer() : ?string
+    {
+        return $this->partData->getAmountIndividualCloudLayerReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get a form of cloud (additional cloud information)
+     * @return string|null
+     */
+    public function getFormCloud() : ?string
+    {
+        return $this->partData->getFormClodReport($this->rawBlocksData);
+    }
+
+    /**
+     * Get height of base of cloud layer (additional cloud information)
+     * @return mixed|null
+     */
+    public function getHeightCloud()
+    {
+        return $this->partData->getHeightCloudReport($this->rawBlocksData);
     }
 
 }
