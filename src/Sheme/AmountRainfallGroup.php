c81606f73ce4f3aac50aa5b18c327bbf0d3e43db
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-03-19 13:00:57 +0200

    updated set and get methods in AmountRainfallGroup class

--- src/Sheme/AmountRainfallGroup.php
+++ src/Sheme/AmountRainfallGroup.php
@@ -53,7 +53,7 @@ class AmountRainfallGroup implements GroupInterface
      * @param string $data Amount of rainfall group data
      * @throws Exception
      */
-    public function setData(string $data)
+    public function setData(string $data) : void
     {
         if (!empty($data)) {
             $this->setRawAmountRainfall($data);
