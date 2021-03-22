ee1a0ed98170c44594a99153a28abb3668916582
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-03-22 17:25:19 +0200

    updated GroupDecoderInterface

--- src/Decoder/GroupDecoder/GroupDecoderInterface.php
+++ src/Decoder/GroupDecoder/GroupDecoderInterface.php
@@ -3,10 +3,18 @@
 namespace Synop\Decoder\GroupDecoder;
 
 /**
+ * The interface should be implemented in a class that performs decoding of individual groups
+ * of this section of the weather report
  *
  * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
  */
 interface GroupDecoderInterface
 {
-    //
+    /**
+     * Checks the validity of the specified group.
+     * The check is whether the intended group is desensitized.
+     *
+     * @return bool
+     */
+    public function isGroup() : bool;
 }
