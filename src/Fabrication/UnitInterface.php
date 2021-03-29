28238a7e7b4efe57b634209af1171304c46a984d
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-03-29 16:59:13 +0300

    updated UnitInterface

--- src/Fabrication/UnitInterface.php
+++ src/Fabrication/UnitInterface.php
@@ -2,10 +2,32 @@
 
 namespace Synop\Fabrication;
 
+use Synop\Sheme\GroupInterface;
+
 /**
+ * UnitInterface must be implemented by a class that works with the units of measurement of meteorological parameters
+ * of all groups of the weather report
  *
  * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
  */
-interface UnitInterface {
-    //put your code here
+interface UnitInterface
+{
+    /**
+     * Sets the unit of measurement for the group parameter to other than the default
+     *
+     * @param string $group Fully-qualified name for a class of group of weather report
+     * @param string $parameter Meteorological parameter of this weather report group
+     * @param string $value Parameter value
+     */
+    public function setUnit(string $group, string $parameter, string $value) : void;
+
+    /**
+     * Returns the unit of measure for all parameters of the given weather group
+     *
+     * array['parameter 1' => 'value of parameter 1', 'parameter 2' ...]
+     *
+     * @param string $group Fully-qualified name for a class of group of weather report
+     * @return array|null
+     */
+    public function getUnitByGroup(string $group) : ?array;
 }
